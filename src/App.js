74923e3c8cdc3bfb82000f2d621c0e5c57745b80
Alexandre Oliveira <alexandre.osantos253@gmail.com>
2023-03-30 15:58:10 -0300

    Requisito 7 Completo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <SolarSystem
           data="solar-system"
         />
+        <Missions />
       </div>
     );
   }
