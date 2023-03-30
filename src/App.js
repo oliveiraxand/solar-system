daee9a6f7d3e408bb588499274140560b3aa18c8
Alexandre Oliveira <alexandre.osantos253@gmail.com>
2023-03-30 15:15:45 -0300

    Requisito 3 Completo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,9 @@ class App extends React.Component {
         <Header />
         <SolarSystem
           data="solar-system"
+        />
+        <Title
+          headline="Planetas"
         />
       </div>
     );
